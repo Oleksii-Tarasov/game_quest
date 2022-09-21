5983761ca018e2a5d5c383e6cd27001c8034706d
Oleksii-Tarasov <alekseytarasov85@gmail.com>
2022-09-21 16:51:29 +0300

    Finished tests for GameConstructor class.

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ _Game Options:_
 - replay the game after winning or losing.
 ___
 ### Warnings
-Some locations feature music, sounds and animated screamers.
+Some locations feature sounds and animated screamers.
 ___
 ### Application launch
 **_1 way - Follow the link:_** https://gamequest.fly.dev/escape_quest/
@@ -22,7 +22,7 @@ ___
 **_2 way - Run on your local computer:_** `escape_quest.war` file in the `dist` project directory needs to be 
 deployed into tomcat 9 version. Then use context to run: http://localhost:8080/escape_quest/ 
 ___
-### Description of classes
+### Class description
 The package `configuration` contains the classes:
 - `ItemProperties` - describes the properties of game items;
 - `LocationProperties` - describes the properties of game locations;
@@ -34,3 +34,19 @@ The package `constant` contains the classes:
 
 The package `model` contains the classes:
 - `Character`, `Location`, `Item` - classes of appropriate game models.
+
+The package `service` contains the classes:
+- `GameConstructor` - creates character, locations Map and items Map;
+- `GameMaster` - manages game mechanics and models;
+- `ResourceLoader` - resource load class to create locations and items from a json files.
+
+The package `servlet` contains the classes:
+- `AppContextListener` - creates attribute that stores an instance of the GameMaster class for further application management;
+- `BossFight` - servlet receives an itemId from the Get request and uses it in the boss fight;
+- `GameRestart` - reset game characteristics to default. Starting a new game session;
+- `GameStart` - servlet displays the "welcome page". Creates game locations, items and character;
+- `ItemServlet` - provides interaction with game items;
+- `LocationFilter` - sets additional rules and attributes on specific locations;
+- `LocationServlet` - assembles a location to display it on a page with all the necessary elements. 
+
+### Resource description
--- src/main/test/java/ua/com/javarush/escape_quest/service/GameConstructorTest.java
+++ src/main/test/java/ua/com/javarush/escape_quest/service/GameConstructorTest.java
@@ -1,14 +1,72 @@
 package ua.com.javarush.escape_quest.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import ua.com.javarush.escape_quest.model.Character;
+import ua.com.javarush.escape_quest.model.Item;
+import ua.com.javarush.escape_quest.model.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameConstructorTest {
+    private final GameConstructor gameConstructor = new GameConstructor(new ResourceLoader());
+
+    @ParameterizedTest
+    @MethodSource("argumentsForCreateLocationsTest")
+    void testCreateLocations_ShouldCreateLocationsFromTemplate(String locationId, String storyBlock, String image, String sound, List<String> itemsInLocation) {
+        Location expectedLocation = new Location(locationId, storyBlock, image, sound, itemsInLocation);
+
+        Map<String, Location> actualLocationMap = gameConstructor.createLocations();
+
+        Location actualLocation = actualLocationMap.get(locationId);
+
+        assertEquals(expectedLocation.getLocationId(), actualLocation.getLocationId());
+        assertEquals(expectedLocation.getStoryBlock(), actualLocation.getStoryBlock());
+        assertEquals(expectedLocation.getImage(), actualLocation.getImage());
+        assertEquals(expectedLocation.getSound(), actualLocation.getSound());
+        assertEquals(expectedLocation.getItemsInLocation(), actualLocation.getItemsInLocation());
+    }
+
+    static Stream<Arguments> argumentsForCreateLocationsTest() {
+        return Stream.of(
+                //location with sound/without item
+                Arguments.of("prison", "/view/prison.jsp", "/img/prison.jpg", "/sound/prison.mp3", new ArrayList<>()),
+                //location without sound/with item
+                Arguments.of("mainhall", "/view/mainhall.jsp", "/img/mainhall.jpg", "", List.of("sword"))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("itemsForCreateItemsTest")
+    void testCreateItems_ShouldCreateItemsFromTemplate(String itemId, Item expectedItem) {
+        Map<String, Item> actualItemMap = gameConstructor.createItems();
+
+        Item actualItem = actualItemMap.get(itemId);
+
+        assertEquals(expectedItem.getItemId(), actualItem.getItemId());
+        assertEquals(expectedItem.getDescription(), actualItem.getDescription());
+        assertEquals(expectedItem.getEffect(), actualItem.getEffect());
+    }
+
+    static Stream<Arguments> itemsForCreateItemsTest() {
+        Item item1 = new Item("pizza", "Pizza slice", "Lord of the Underworld is not hungry");
+        Item item2 = new Item("book", "Poetry book", "You recite a verse from a book, but the Lord of the Underworld does not get kinder");
+
+        return Stream.of(
+                Arguments.of("pizza", item1),
+                Arguments.of("book", item2)
+        );
+    }
+
     @Test
     void testCreateCharacter_ShouldCreateNewCharacterWithChosenNickName() {
-        GameConstructor gameConstructor = new GameConstructor(new ResourceLoader());
         //create first character:
         Character expectedFirstCharacter = new Character(1, "user1", 3);
 
